f9ff7ce2058004e2f438c6f5c31162f6f6d76497
Abdirahman04 <abdixcom@gmail.com>
2024-02-25 16:03:59 +0300

    added init method in main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/Abdirahman04/bytebank-gin/internal/api"
+import (
+  "log"
+
+  "github.com/Abdirahman04/bytebank-gin/internal/api"
+  "github.com/joho/godotenv"
+)
+
+func init() {
+  err := godotenv.Load()
+
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+}
 
 func main() {
   api.Start()

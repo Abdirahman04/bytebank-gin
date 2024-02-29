8a667b3cd90697b47aee9dc23e4743272194f55e
Abdirahman04 <abdixcom@gmail.com>
2024-02-29 14:19:06 +0300

    added Post method in customer handler

--- internal/customer/handler.go
+++ internal/customer/handler.go
@@ -1,1 +1,15 @@
 package customer
+
+import "github.com/gin-gonic/gin"
+
+func Post(c *gin.Context) {
+  res, err := SaveCustomer()
+  if err != nil {
+    c.Status(400)
+    return
+  }
+  
+  c.JSON(200, gin.H{
+    "result": res,
+  })
+}

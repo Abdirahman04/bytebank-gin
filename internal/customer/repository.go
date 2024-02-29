c991d55fdabf2425b8fa6e7084479f1a0e10319a
Abdirahman04 <abdixcom@gmail.com>
2024-02-29 13:55:45 +0300

    added Save method in customer repository

--- internal/customer/repository.go
+++ internal/customer/repository.go
@@ -1,1 +1,19 @@
 package customer
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Abdirahman04/bytebank-gin/pkg/db"
+)
+
+func Save(customer Customer) (string, error) {
+  db, err := db.ConnectToDb()
+  if err != nil {
+    return "", errors.New("Error connecting to db")
+  }
+
+  result := db.Create(&customer)
+  
+  return fmt.Sprintln(result.RowsAffected), nil
+}

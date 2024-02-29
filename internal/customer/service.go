eb57b005e1553d7f245039f711b43221b8c0dbf4
Abdirahman04 <abdixcom@gmail.com>
2024-02-29 14:14:25 +0300

    added SaveCustomer method in customer service

--- internal/customer/service.go
+++ internal/customer/service.go
@@ -1,1 +1,8 @@
 package customer
+
+func SaveCustomer() (string, error) {
+  customer := Customer{FirstName: "Abdirahman", LastName: "Hassan", Email: "abdix@gmail", Password: "killmonger"}
+
+  res, err := Save(customer)
+  return res, err
+}

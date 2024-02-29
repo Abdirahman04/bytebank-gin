51f74efdbfc1a57e1800b570dcba760b0bcc0eb5
Abdirahman04 <abdixcom@gmail.com>
2024-02-29 17:46:57 +0300

    added CustomerResponse struct

--- internal/customer/customer.go
+++ internal/customer/customer.go
@@ -1,6 +1,10 @@
 package customer
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Customer struct {
   gorm.Model
@@ -17,11 +21,35 @@ type CustomerRequest struct {
   Password string `json:"password"`
 }
 
+type CustomerResponse struct {
+  Id uint `json:"id"`
+  FirstName string `json:"firstname"`
+  LastName string `json:"lastname"`
+  Email string `json:"email"`
+  Password string `json:"password"`
+  CreatedAt time.Time `json:"createdat"`
+  UpdatedAt time.Time `json:"updatedat"`
+  DeletedAt time.Time `json:"deletedat"`
+}
+
 func NewCustomer(customer CustomerRequest) Customer {
   return Customer{
     FirstName: customer.FirstName,
     LastName: customer.LastName,
     Email: customer.Email,
     Password: customer.Password,
+  }
+}
+
+func NewCustomerResponse(customer Customer) CustomerResponse {
+  return CustomerResponse{
+    Id: customer.ID,
+    FirstName: customer.FirstName,
+    LastName: customer.LastName,
+    Email: customer.Email,
+    Password: customer.Password,
+    CreatedAt: customer.CreatedAt,
+    UpdatedAt: customer.UpdatedAt,
+    DeletedAt: customer.DeletedAt.Time,
   }
 }

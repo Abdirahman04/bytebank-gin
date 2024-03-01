b6224a7486a0470cdeeae85ceae8c94dbf092e91
Abdirahman04 <abdixcom@gmail.com>
2024-03-01 20:00:01 +0300

    added AccountRequest struct

--- internal/account/account.go
+++ internal/account/account.go
@@ -5,6 +5,12 @@ import "gorm.io/gorm"
 type Account struct {
   gorm.Model
   CustomerId uint
-  AccountType uint
+  AccountType string
   Amount float32
+}
+
+type AccountRequest struct {
+  CustomerId uint `json:"customerid"`
+  AccountType string `json:"accounttype"`
+  Amount float32 `json:"amount"`
 }

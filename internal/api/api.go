02bbc4ed6d9ac778dfd87e833f8ba93193a1e4ec
Abdirahman04 <abdixcom@gmail.com>
2024-03-01 19:34:15 +0300

    added some code

--- internal/api/api.go
+++ internal/api/api.go
@@ -12,6 +12,7 @@ func Start() {
   r.POST("/customer", customer.Post)
   r.GET("/customer", customer.GetAll)
   r.GET("/customer/:id", customer.GetOne)
+  r.PUT("/customer/:id", customer.UpdateOne)
 
   r.Run()
 }

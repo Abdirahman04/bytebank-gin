2735375ec33b2370d4b852dec3a2a2e56c8fffa8
Abdirahman04 <abdixcom@gmail.com>
2024-03-02 14:28:20 +0300

    added Account migrate

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,10 +1,12 @@
 package migrate
 
 import (
+	"github.com/Abdirahman04/bytebank-gin/internal/account"
 	"github.com/Abdirahman04/bytebank-gin/internal/customer"
 	"github.com/Abdirahman04/bytebank-gin/pkg/db"
 )
 
 func Migrate() {
   db.DB.AutoMigrate(&customer.Customer{})
+  db.DB.AutoMigrate(&account.Account{})
 }

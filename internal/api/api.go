d7043118c043be2e5bf01fc10000df13bd99c076
Abdirahman04 <abdixcom@gmail.com>
2024-03-02 14:47:03 +0300

    added account endpoints

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/Abdirahman04/bytebank-gin/internal/account"
 	"github.com/Abdirahman04/bytebank-gin/internal/customer"
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +14,9 @@ func Start() {
   r.GET("/customer", customer.GetAll)
   r.GET("/customer/:id", customer.GetOne)
   r.PUT("/customer/:id", customer.UpdateOne)
+
+  r.POST("/account", account.Post)
+  r.GET("/account", account.GetAll)
 
   r.Run()
 }

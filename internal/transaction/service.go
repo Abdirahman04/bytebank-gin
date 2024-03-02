c577da95c23dcfda8f835d5ed302998a2c091339
Abdirahman04 <abdixcom@gmail.com>
2024-03-02 16:53:10 +0300

    added GetAllTransactions method in transaction service

--- internal/transaction/service.go
+++ internal/transaction/service.go
@@ -11,3 +11,20 @@ func SaveTransaction(transaction TransactionRequest) (TransactionResponse, error
   resTransaction := NewTransactionResponse(res)
   return resTransaction, nil
 }
+
+func GetAllTransactions() ([]TransactionResponse, error) {
+  var transactions []TransactionResponse
+
+  res, err := GetAll()
+  if err != nil {
+    return nil, err
+  }
+
+  for _, transaction := range res {
+    newTransaction := NewTransactionResponse(transaction)
+
+    transactions = append(transactions, newTransaction)
+  }
+
+  return transactions, nil
+}

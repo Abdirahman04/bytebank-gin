07142006e77fe30807a2f64dd83ace138aa53118
Abdirahman04 <abdixcom@gmail.com>
2024-03-08 21:28:18 +0300

    added some code

--- validations/validate_transactions.go
+++ validations/validate_transactions.go
@@ -1,7 +1,7 @@
 package validations
 
 func CheckTransactionType(typ string) bool {
-  if typ == "deposit" || typ == "withdraw" || typ == "transafer" {
+  if typ == "deposit" || typ == "withdraw" || typ == "transfer" {
     return true
   } else {
     return false
